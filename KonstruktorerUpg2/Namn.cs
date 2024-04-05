d031d28cff8dc4ce05dfafb94b77aa70b6c5504d
Airblow <143172177+Airblow@users.noreply.github.com>
2024-04-05 13:31:46 +0200

    Add project files.

---
+++ KonstruktorerUpg2/Namn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonstruktorerUpg2
+{
+    class Namn
+    {
+        private string förnamn;
+        private string efternamn;
+
+        public string NamnFörnamn
+        {
+            get{ return förnamn; }
+
+            set{ förnamn = value; }
+        }
+
+        public string NamnEfternamn
+        {
+            get { return efternamn; }
+
+            set { efternamn = value; }
+        }
+
+        public void NamnOutput(string förnamn, string efternamn)
+        {
+            Console.WriteLine($"Du skrev in ditt namn som: {förnamn} och {efternamn}");
+
+        }
+
+    }
+}
---
+++ KonstruktorerUpg2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace KonstruktorerUpg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Namn p1 = new Namn();
+
+            bool failsafe = false;
+
+            while (failsafe != true)
+            {
+                try
+                {
+                    Console.WriteLine("Vad är ditt förnman?");
+                    p1.NamnFörnamn = Console.ReadLine();
+                    Console.Clear();
+
+                    Console.WriteLine("Vad är ditt efternamn?");
+                    p1.NamnEfternamn = Console.ReadLine();
+                    Console.Clear();
+
+                    p1.NamnOutput(p1.NamnFörnamn, p1.NamnEfternamn);
+                }
+                catch
+                {
+                    Console.WriteLine("Något gick fel, försök igen.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            Console.ReadKey();
+
+        }
+        
+    }
+
+}
